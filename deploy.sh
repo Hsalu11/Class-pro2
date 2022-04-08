8ada8d3d0e4696f730644ddd52d7839adb739165
upinfo <mholder.tiyoil@gmail.com>
2022-04-08 16:31:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 pwd
 pm2 start "ng serve --host 0.0.0.0 --port 4200" --name "Angular App" #This will start angular app
+pm2 logs Angular App
 cd API
 pm2 start --name "Nodejs API" npm -- start  
+pm2 logs Nodejs API
+
+pm2 startup systemd
