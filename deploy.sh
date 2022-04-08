aa01371ffba16a3457d009f8269bb06684c74568
upinfo <mholder.tiyoil@gmail.com>
2022-04-08 16:57:10 +0530

    Final cicd pipeline

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,5 @@ pm2 start --name "Nodejs API" npm -- start #This will start Nodejs app
 # Startup command
 sudo env PATH=$PATH:/home/ubuntu/actions-runner/angular-app/_tool/node/14.19.1/x64/bin /home/ubuntu/actions-runner/angular-app/_tool/node/14.19.1/x64/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 pm2 save
+
+# Working workflow
